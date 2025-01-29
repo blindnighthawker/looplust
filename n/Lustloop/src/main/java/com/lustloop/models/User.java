d87efd0baf96e6c1084681ea1e15e93b8a8f3656
coolassites <matheusniehues_4@hotmail.com>
2025-01-29 15:41:07 -0300

    Add files via upload

---
+++ n/Lustloop/src/main/java/com/lustloop/models/User.java
@@ -0,0 +1,23 @@
+package com.lustloop.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @ElementCollection
+    private Set<String> roles = new HashSet<>();
+}
