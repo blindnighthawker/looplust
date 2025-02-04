b6d05357154d75db5b1319e408ac18651e365d75
coolassites <matheusniehues_4@hotmail.com>
2025-02-04 02:09:08 -0300

    Add files via upload

---
+++ swipesin/Lustloop/src/main/java/com/lustloop/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+public class UserRepository {
+    
+}
+package com.lustloop.repositories;
+
+import com.lustloop.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
